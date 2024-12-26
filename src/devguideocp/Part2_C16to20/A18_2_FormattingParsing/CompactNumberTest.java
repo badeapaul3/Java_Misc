18a530dd58bc4c741599eaf848f34473edc73a39
Paul Badea <badeapaul3@gmail.com>
2024-12-26 19:41:09 +0200

    Chapter 18 progress

---
+++ src/devguideocp/Part2_C16to20/A18_2_FormattingParsing/CompactNumberTest.java
@@ -0,0 +1,33 @@
+package devguideocp.Part2_C16to20.A18_2_FormattingParsing;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class CompactNumberTest {
+    public static void main(String[] args) {
+
+        NumberFormat shortCompactFormat = NumberFormat.getCompactNumberInstance(
+                Locale.US, NumberFormat.Style.SHORT);
+        NumberFormat longCompactFormat = NumberFormat.getCompactNumberInstance(
+                Locale.US, NumberFormat.Style.LONG);
+
+        System.out.println(shortCompactFormat.format(9_400_000));    // 9M
+        System.out.println(longCompactFormat.format(9_400_000));     // 9 million
+
+        shortCompactFormat.setMaximumFractionDigits(2);
+        longCompactFormat.setMaximumFractionDigits(2);
+
+        System.out.println(shortCompactFormat.format(9_400_000));    // 9M
+        System.out.println(longCompactFormat.format(9_400_000));     // 9 million
+        try {
+            System.out.println(shortCompactFormat.parse("9M"));        // 9000000
+            System.out.println(longCompactFormat.parse("9 million"));  // 9000000
+        } catch (ParseException pe) {
+            System.out.println(pe);
+        }
+
+
+
+    }
+}
