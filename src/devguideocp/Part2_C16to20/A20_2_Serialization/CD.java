da4d485e6f5979a9c81097ea65773cd5dd330250
Paul <badeapaul3@gmail.com>
2025-01-06 11:35:24 +0200

    Chapter 20 progress

---
+++ src/devguideocp/Part2_C16to20/A20_2_Serialization/CD.java
@@ -0,0 +1,12 @@
+package devguideocp.Part2_C16to20.A20_2_Serialization;
+
+/**
+ * @author hatzp
+ **/
+import java.io.Serializable;
+import java.time.Year;
+/** A record class that represents a CD. */
+public record CD(String artist, String title, int noOfTracks,
+                 Year year, Genre genre) implements Serializable {
+    public enum Genre implements Serializable {POP, JAZZ, OTHER}
+}
